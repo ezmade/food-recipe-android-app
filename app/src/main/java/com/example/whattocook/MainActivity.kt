91b01e6b3aa8d884973448888d6f9788ccaeaa36
Anton Boyko <abpoet@mail.ru>
2020-12-08 15:52:55 +0500

    Replaced findViewById to viewBinding and fixed color scheme

--- app/src/main/java/com/example/whattocook/MainActivity.kt
+++ app/src/main/java/com/example/whattocook/MainActivity.kt
@@ -3,22 +3,23 @@ package com.example.whattocook
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import com.example.whattocook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val btnViewRecipe: Button = findViewById(R.id.btnViewRecipe)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        btnViewRecipe.setOnClickListener {
+        binding.btnViewRecipe.setOnClickListener {
             val recipe = Recipe("Carbonara", "Pasta", 120)
-
             val intent = Intent(this, RecipeDetailActivity::class.java)
             intent.putExtra("Recipe", recipe)
             startActivity(intent)
         }
     }
-
-
 }
--- app/src/main/java/com/example/whattocook/RecipeDetailActivity.kt
+++ app/src/main/java/com/example/whattocook/RecipeDetailActivity.kt
@@ -2,20 +2,22 @@ package com.example.whattocook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import com.example.whattocook.databinding.ActivityRecipeDetailBinding
 
 class RecipeDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRecipeDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recipe_detail)
+        binding = ActivityRecipeDetailBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val recipeName: TextView = findViewById(R.id.recipeName)
-        val recipeCategory: TextView = findViewById(R.id.recipeCategory)
-        val recipeTotalPrice: TextView = findViewById(R.id.recipeTotalPrice)
         val recipe = intent.extras?.getSerializable("Recipe") as Recipe
 
-        recipeName.text = "Meal: ${recipe.name}"
-        recipeCategory.text = "Category: ${recipe.category}"
-        recipeTotalPrice.text = "Price: ${recipe.totalPrice}"
+        binding.recipeName.text = "Meal: ${recipe.name}"
+        binding.recipeCategory.text = "Category: ${recipe.category}"
+        binding.recipeTotalPrice.text = "Price: ${recipe.totalPrice}"
     }
 }
